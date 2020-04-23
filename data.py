7a5b96f5312c279436769d1c8f2ac70e00e51e20
Bruce Crevensten <bruce.crevensten@gmail.com>
2020-04-22 19:16:27 -0700

    Adds cache module so external API only gets hit once per day.

--- data.py
+++ data.py
@@ -1,33 +1,78 @@
-# import urllib.parse
-import pandas as pd
-import datetime
+"""
+Responsible for fetching, preprocessing
+and caching community data.
+"""
+# pylint: disable=C0103, E0401
 
-def fetch_data(community):
+import urllib.parse
+import os
+import datetime
+import logging
+import pandas as pd
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+
+DASH_LOG_LEVEL = os.getenv("DASH_LOG_LEVEL", default="info")
+logging.basicConfig(level=getattr(logging, DASH_LOG_LEVEL.upper(), logging.INFO))
+
+API_URL = os.getenv("ACIS_API_URL", default="http://data.rcc-acis.org/StnData?")
+logging.info("Using ACIS API url %s", API_URL)
+
+# Set up cache.
+CACHE_EXPIRE = int(os.getenv("DASH_CACHE_EXPIRE", default="43200"))
+logging.info("Cache expire set to %s seconds", CACHE_EXPIRE)
+cache_opts = {"cache.type": "memory"}
+
+cache = CacheManager(**parse_cache_config_options(cache_opts))
+data_cache = cache.get_cache("api_data", type="memory", expire=CACHE_EXPIRE)
+
+
+def fetch_api_data(community):
     """
     Reads data from ACIS API for selected community.
     """
+    logging.info("Sending upstream data API request")
+    query = urllib.parse.urlencode(
+        {
+            "sid": community,
+            "sdate": "1950-01-01",
+            "edate": datetime.date.today().strftime("%Y-%m-%d"),
+            "elems": "4,10",  # precip & snow!
+            "output": "csv",
+        }
+    )
+    query = API_URL + query
+    logging.debug("API query string: %s", query)
+    std = pd.read_csv(
+        query, names=["date", "pcpt", "snow"], parse_dates=True, skiprows=1
+    )
 
-    # Placeholder for early dev/explore work.
-    # This code works for fetching from the API.
-    # TODO we must cache the results for a community here,
-    # so that we're at least not hitting the API up twice
-    # for every page load / location change.
-    # https://beaker.readthedocs.io/en/latest/index.html or similar.
+    # Drop missing, switch trace to 0, and assign column types
+    std = std.loc[std["pcpt"] != "M"]  # drop missing
+    std = std.loc[std["snow"] != "M"]  # drop missing
+    std = std.replace("T", 0)  # make T (Trace) = 0
+    std["pcpt"] = std["pcpt"].astype("float")
+    std["snow"] = std["snow"].astype("float")
 
-    # query = urllib.parse.urlencode(
-    #     {"sid": "26451", "sdate": "1950-01-01", "edate": "2020-04-20", "elems": "4,10", "output": "csv"}
-    # )
-    # api_url = "http://data.rcc-acis.org/StnData?"
-    # std = pd.read_csv(api_url + query, names=["date", "pcpt", "snow"], parse_dates=True, skiprows=1)
-    # std = std.loc[std.pcpt != "M"]  # drop missing
-    # std = std.loc[std.snow != "M"]  # drop missing
-    # std = std.replace("T", 0)  # make T (Trace) = 0
-    # std.to_csv("data/anchorage.csv")
+    # Remove 0's.
+    std = std.loc[(std["pcpt"] > 0) | (std["snow"] > 0)]
 
-    std = pd.read_csv("data/anchorage.csv")
-    std = std.loc[std.pcpt > 0]
     std["date"] = pd.to_datetime(std["date"])
     std["doy"] = std["date"].apply(lambda d: d.strftime("%j")).astype("int")
     std["year"] = std["date"].apply(lambda d: d.strftime("%Y")).astype("int")
     std["total"] = std["pcpt"] + std["snow"]
     return std
+
+
+def fetch_data(community):
+    """
+    Fetches preprocessed data from cache,
+    or triggers an API request + preprocessing.
+    """
+
+    # Wrapper to call lambda (fetch_api_data) with argument
+    # within scope here.
+    def call_api():
+        return fetch_api_data(community)
+
+    return data_cache.get(key=community, createfunc=call_api)
